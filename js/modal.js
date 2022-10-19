cb071265e67a1f5bcf8a05a7b1b8463f27ad6aa4
Oleksandr Zubarev <ozubarev333@gmail.com>
2022-10-19 10:54:37 +0200

    fixed mistakes

--- js/modal.js
+++ js/modal.js
@@ -10,6 +10,7 @@
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
+    document.body.classList.toggle("modal-open")
     refs.modal.classList.toggle("backdrop--is-hidden");
   }
 })();
